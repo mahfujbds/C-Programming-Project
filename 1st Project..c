1a5a8670b3f03b1f7a2577c3f26f82c4c118721e
mahfujbds <mahfujtarek@yahoo.com>
2024-11-21 22:06:41 -0500

    I corrected this program.

--- 1st Project..c
+++ 1st Project..c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    printf("Welcome to the simple Fintness Tracking System!\n");
+    printf("Welcome to the simple Fitness Tracking System!\n");
     printf("\nFitness Tracking Menu: Select an option ");
     printf("\n1: Log Workout\n2: Check Total Calories Burned\n3: Check Goal Status");
 
     int option; //we took a variable for option
-    scanf("%d", Option);
+    scanf("%d", &option);
 
 
-    while(Option!=4)
-        if (Option == 1) //== means it will read small or capital latter also.
+    while(option!=4)
+        if (option == 1) //== means it will read small or capital latter also.
         {
             printf("Select exercise type:\n");
-            printf("1: Running-10 Calories/min\n:")
+            printf("1: Running-10 Calories/min\n:");
 
         }
 
-        else if (Option == 2)
-
-        {
-
-        }
-
-        else if (option == 3)
-
-        {
-
-        }
 
     printf("Exit The Program");
 
